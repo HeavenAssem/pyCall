aa6fd9796ad357638f3ca87feab3965d75fa1b61
Vladimir Kanaev <v.kanaev2112@gmail.com>
2014-11-05 23:22:58 +0300

    Made video translation

--- transmit_image.py
+++ transmit_image.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy
 import socket
 import time
@@ -6,27 +6,28 @@ import sys
 
 start = time.time()
 
+cap = cv.VideoCapture(0)
+
 transmitter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 transmitter.connect(('127.0.0.1', 9091))
 
-img = cv2.imread('test1.jpg')
-img_size = img.size
-count = int(img_size/20480)
+while True:
+  ret, img = cap.read()
+  img_size = img.size
+  count = int(img_size/20480)
+  print str(img.shape), img.size
+  transmitter.send(str(img.shape))
+  transmitter.send(str(count))
+  for i in range(count):
+    #print i
+    transmitter.send(img.data[20480*i:(i+1)*20480])
+    print len(img.data[20480*(count):])
+  transmitter.send(img.data[20480*(count):])
 
-print img.itemsize
-print len(img)
-print img_size, count
-print str(img.shape)
-print img.dtype
 
+ransmitter.close()
 
-transmitter.send(str(img.shape))
-transmitter.send(str(count))
-for i in range(count):
-  transmitter.send(img.data[20480*i:(i+1)*20480])
-transmitter.send(img.data[20480*(count):])
-transmitter.close()
 
 end = time.time()
 print end-start
