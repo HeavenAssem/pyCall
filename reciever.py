1bdf9b32489c32071a517911c804d31c9595d1db
Vladimir <v.kanaev2112@gmail.com>
2014-11-05 17:41:31 +0300

    Made transmittion of the image through the net

--- reciever.py
+++ reciever.py
@@ -1,36 +1,45 @@
 import socket
 import numpy as np
 import binascii
+import time
 import sys
+import cv2 as cv
+
+
+start = time.time()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind(('', 9091))
 sock.listen(1)
 
+print time.time()-start
 reciever, addr = sock.accept()
 
 shape = reciever.recv(13)
+count = reciever.recv(2)
+
 
 splitted = shape.split(',')
 x1, x2, x3 = int(splitted[0][1:]) , int(splitted[1][1:]) , int(splitted[2][:len(splitted[2])-1])
 
+print time.time()-start
 
-#buf = buffer(reciever.recv(x1*x2*x3))
-buf = buffer(reciever.recv(20480))
-
-print x1, x2, x3, x1*x2*x3, sys.getsizeof(buf), len(buf)
+buf = ''
+for i in range(int(count)+1):
+  buf = buf + reciever.recv(20480)
 
 aar = []
 for ch in buf:
   aar.append(ord(ch))
 
 arr = np.array(aar)
-print type(aar), len(aar)
-print type(arr), arr.size
-#ppp = arr.reshape((x1, x2, x3))
+ppp = arr.reshape((x1, x2, x3))
 
-#print len(ppp)
+cv.imwrite('recieved_image.jpg', ppp)
+
+print ppp.shape
 
 reciever.close()
 
+print time.time()-start
--- transmit_image.py
+++ transmit_image.py
@@ -1,21 +1,32 @@
 import cv2
 import numpy
 import socket
+import time
 import sys
+
+start = time.time()
 
 transmitter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 transmitter.connect(('127.0.0.1', 9091))
 
 img = cv2.imread('test1.jpg')
+img_size = img.size
+count = int(img_size/20480)
 
 print img.itemsize
 print len(img)
-print img.size
+print img_size, count
 print str(img.shape)
 print img.dtype
 
 
 transmitter.send(str(img.shape))
-transmitter.send(img.data[0:20480])
+transmitter.send(str(count))
+for i in range(count):
+  transmitter.send(img.data[20480*i:(i+1)*20480])
+transmitter.send(img.data[20480*(count):])
 transmitter.close()
+
+end = time.time()
+print end-start
